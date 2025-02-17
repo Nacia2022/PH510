af0d86ca8058bb919e1149287755e02fdb85f6e7
Nacia2022 <nkoziol71@gmail.com>
2025-02-17 16:15:33 +0000

    Pylint fixes
    Fixes for improved pylint score.

--- Task 2/vector.py
+++ Task 2/vector.py
@@ -1,24 +1,25 @@
-"""
-   Toy example code for a very simple object oriented class of vectors
-"""
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Toy example code for a very simple object oriented class of vectors."""
+
+# Add postponed annotations feature to use something in an annotation even if
+# it hasnt been defined yet.
+from __future__ import annotations
+
 
 class Vector:
-    """
-       Vector class for two dimensional quantities
-    """
-    def __init__(self, a, b):
-        self.a = a
-        self.b = b
+    """Vector class for two dimensional quantities (2D vector)."""
+
+    def __init__(self, x_arg, y_arg):
+        """Initialize a vector with components x and b."""
+        self.x_arg = x_arg
+        self.y_arg = y_arg
 
     def __str__(self):
-        """
-        Assumes floating point when printing
-        """
-        return f"Vector: ({self.a:.2f}, {self.b:.2f})"
+        """Assumes floating point when printing."""
+        return f"Vector: ({self.x_arg:.2f}, {self.y_arg:.2f})"
 
-    def __add__(self,other):
-        """
-        Overloads addition for the elements of two instances
-
-        """
-        return Vector(self.a + other.a, self.b + other.b)
+    def __add__(self, other):
+        """Overloads addition for the elements of two instances."""
+        return Vector(self.x_arg + other.x_arg, self.y_arg + other.y_arg)
