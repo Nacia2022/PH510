fa11684491d5195034cfa8cbec0669a4a75c814e
Nacia2022 <nkoziol71@gmail.com>
2025-02-17 16:20:57 +0000

    Correcting and testing test code

--- Task 2/test.py
+++ Task 2/test.py
@@ -1,10 +1,17 @@
-"""
-Use the vector class to do something simple
-"""
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Use the vector class to do something simple."""
 
 import vector as vec
-v1 = vec.Vector(2,10)
-print("first vector",v1)
-v2 = vec.Vector(5,-2)
-print("second vector",v2)
+
+# Adding vlues to the components
+
+v1 = vec.Vector(2, 10)
+v2 = vec.Vector(5, -2)
+
+
+# Print values
+print("first vector", v1)
+print("second vector", v2)
 print("sum of vectors:", v1 + v2)
