45c1aaab394eaaf1ce9af6fb90c5814457b182bb
Nacia2022 <nkoziol71@gmail.com>
2025-03-15 21:17:58 +0000

    Prep for Monte Carlo
    Trying to use previous code to set up Monte Carlo class. Using .rng - random number generator.

--- Task_3/Task3_code.py
+++ Task_3/Task3_code.py
@@ -11,73 +11,91 @@ import time
 from mpi4py import MPI, rc
 import numpy as np
 
-comm = MPI.COMM_WORLD
+# comm = MPI.COMM_WORLD
 
-rc.fast_reduce = True # This is the default 9
-# Note the lower case for reduce:
-res = comm.reduce(np.asarray([1,2,3]), op=MPI.SUM)
-
-if comm.Get_rank() == 0:
-print("All done!", res)
+# Create Monte Carlo class for parallel simulations
 
 
-
-
-
-
-
-
-
+class MonteCarlo:
+    def __init__(self, seed=None):
+        """Initate class to setup parellel processing for the Monte Carlo
+        get the process rank and the total number of processes."""
+        self.comm = MPI.COMM_WORLD
+        self.rank = self.comm.Get_rank()
+        self.size = self.comm.Get_size()
+        # Random number generator (rng) for each process (rank). We split the
+        # work into separate processes that will run part of the computation
+        self.rng = np.random.default_rng(seed + self.rank)
 
 
 
+# rc.fast_reduce = True # This is the default 9
+# # Note the lower case for reduce:
+# res = comm.reduce(np.asarray([1,2,3]), op=MPI.SUM)
 
-# Initializing communication in MPI
-comm = MPI.COMM_WORLD
-rank = comm.Get_rank()  # Get the process rank
-size = comm.Get_size()  # Get the total number of processes
-
-# Get the total number of ranks
-nproc = comm.Get_size()
-
-# The first processor is leader (rank 0), so one fewer available to be a worker
-nworkers = nproc - 1
-
-# Integral parameters
-N = 100000  # Number of steps for integration
-DEL = 1.0 / N  # Step size
-INT = 0.0  # Integral
+# if comm.Get_rank() == 0:
+# print("All done!", res)
 
 
-# Define function
-def integrand(x_i):
-    """Program to integrate the function to obtain the value of pi."""
-    return 4.0 / (1.0 + x_i*x_i)
 
-# NEW VERSION
-# Divide work (N) amoung the ranks so that each calculates a partial sum of the integral
-DIV_N = N // size
-start = rank * DIV_N
-end = start + DIV_N
 
-time1 = time.time()
-# Divides integral sum
-DIV_INT = 0.0  # Initialize
-for i in range(start, end):
-    x = (i+0.5) *DEL
-    DIV_INT += integrand(x) * DEL
 
-# Combine all partial sums
-TOTAL_INT = comm.reduce(DIV_INT, op=MPI.SUM, root=0)
 
-# Print result from rank 0
-if rank == 0:
-    print(f"Integral = {TOTAL_INT:.15f}")
 
-time2 = time.time()
 
-time = time2 - time1
 
-print("Total time (seconds) =", time)
 
-# MPI.Finalize()
+
+
+
+
+# CODE FOR TASK 1
+# # Initializing communication in MPI
+# comm = MPI.COMM_WORLD
+# rank = comm.Get_rank()  # Get the process rank
+# size = comm.Get_size()  # Get the total number of processes
+
+# # Get the total number of ranks
+# nproc = comm.Get_size()
+
+# # The first processor is leader (rank 0), so one fewer available to be a worker
+# nworkers = nproc - 1
+
+# # Integral parameters
+# N = 100000  # Number of steps for integration
+# DEL = 1.0 / N  # Step size
+# INT = 0.0  # Integral
+
+
+# # Define function
+# def integrand(x_i):
+#     """Program to integrate the function to obtain the value of pi."""
+#     return 4.0 / (1.0 + x_i*x_i)
+
+# # NEW VERSION
+# # Divide work (N) amoung the ranks so that each calculates a partial sum of the integral
+# DIV_N = N // size
+# start = rank * DIV_N
+# end = start + DIV_N
+
+# time1 = time.time()
+# # Divides integral sum
+# DIV_INT = 0.0  # Initialize
+# for i in range(start, end):
+#     x = (i+0.5) *DEL
+#     DIV_INT += integrand(x) * DEL
+
+# # Combine all partial sums
+# TOTAL_INT = comm.reduce(DIV_INT, op=MPI.SUM, root=0)
+
+# # Print result from rank 0
+# if rank == 0:
+#     print(f"Integral = {TOTAL_INT:.15f}")
+
+# time2 = time.time()
+
+# time = time2 - time1
+
+# print("Total time (seconds) =", time)
+
+MPI.Finalize()
