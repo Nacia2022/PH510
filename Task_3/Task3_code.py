d5629902734ed7c27cd97732ef8565b49edcffab
Nacia2022 <nkoziol71@gmail.com>
2025-03-26 16:48:21 +0000

    Updates - gauss function
    Changes to the method of calculation.  Getting pylint error for local sums - needs to be revisited.

--- Task_3/Task3_code.py
+++ Task_3/Task3_code.py
@@ -13,7 +13,7 @@ Licensed and Copyrighted 2025.
 # import time
 from mpi4py import MPI
 import numpy as np
-# import sys
+
 
 
 # Create Monte Carlo class for parallel simulations
@@ -27,6 +27,7 @@ class MonteCarlo:
         self.comm = MPI.COMM_WORLD
         self.rank = self.comm.Get_rank()
         self.size = self.comm.Get_size()
+
         # Random number generator (rng) for each process (rank). We split the
         # work into separate processes that will run part of the computation.
         # Seed in rng is for reproducability of a random number sequence so we
@@ -36,6 +37,7 @@ class MonteCarlo:
         else:
             new_seed = None
         # Else for no provided seed - get numpy to generate a radom seed.
+
         self.rng = np.random.default_rng(new_seed)
 
         # Generate random numbers
@@ -67,6 +69,7 @@ class MonteCarlo:
 
             return volume_esti, volume_err  # Rank 0 returns estimated volume and error
         return None, None  # Every other rank returns nothing
+
     # Attempt at Gaussian
     def gauss(self, x_1, x_0, sig):
         """Calculate the value of a Gaussian distribution at a point x.
@@ -75,7 +78,12 @@ class MonteCarlo:
         x0: Mean of distribution
         sig: Standard deviation
         """
-        return np.exp(-((x_1-x_0)**2) / (2*sig**2))/ (sig*np.sqrt(2*np.pi))
+        dimens = x_1.shape[1]  # Number of dimensions
+        coef = 1/((2*np.pi*sig**2)**(dimens/2))
+        exponential = -np.sum((x_1 - x_0) ** 2, axis=1) / (2*sig**2)
+        return coef*np.exp(exponential)
+
+        # return np.exp(-((x_1-x_0)**2) / (2 * sig ** 2))/ (sig*np.sqrt(2*np.pi))
 
 
     def gauss_int(self, x_0=0, sig=1, dimensions=1, sample_num=10000):
@@ -87,20 +95,40 @@ class MonteCarlo:
         dimensions: Number of dimensions
         
         samples: draw samples for MC  from the given range -10 sigma to 10 sigma in given dimension.
+        Excluding regions of the function that are effectively 0.
         Parallization to spread samples between ranks.
         """
+        x_0 = np.full(dimensions, x_0)  # So that its a vector in dimensions over 1
+
+        #
         samples = self.rng.uniform(-10 * sig, 10 * sig, (sample_num // self.size, dimensions))
-        values = np.prod(self.gauss(samples, x_0, sig), axis=1)
-        local_sum = np.sum(values)
-        local_sum_sq = np.sum(values ** 2)
-        total = self.comm.reduce(local_sum, op=MPI.SUM, root=0)
-        total_sq = self.comm.reduce(local_sum_sq, op=MPI.SUM, root=0)
+
+        # Evaluate function at sampled point with axis for dimensions over 1.
+        values = self.gauss(samples, x_0, sig)
+
+        # Estimation of integral over range. Calculate sum of all functions and functions
+        # squared at points.
+        local_int = np.sum(values)
+        local_mean = np.sum(samples * values[:, np.newaxis], axis=0)  # x*f(x)
+        local_mean_sq = np.sum((samples**2) * values[:, np.newaxis], axis=0)  # x**2*f(x))
+        # local_sum_sq = np.sum(values ** 2)
+
+        # Reduce all sums at rank 0.
+        total_int = self.comm.reduce(local_int, op=MPI.SUM, root=0)
+        total_mean = self.comm.reduce(local_mean, op=MPI.SUM, root=0)
+        total_mean_sq = self.comm.reduce(local_mean_sq, op=MPI.SUM, root=0)
+        # total_sq = self.comm.reduce(local_sum_sq, op=MPI.SUM, root=0)
+
         if self.rank ==0:
-            mean = (total / sample_num) * (20 * sig) ** dimensions
-            variance = ((total_sq / sample_num) - (mean ** 2)) * (20 * sig) ** (2 * dimensions)
-            gauss_err = np.sqrt(mean/sample_num)
-            return mean, variance, gauss_err
-        return None, None, None
+            # Volume of region
+            vol_reg = (20 * sig) ** dimensions
+
+            integral = (total_int / sample_num) * vol_reg
+            mean = total_mean / total_int
+            variance = ((total_mean_sq / total_int) - (mean ** 2)) * vol_reg
+            gauss_err = np.sqrt(values) * vol_reg / np.sqrt(sample_num)
+            return integral, mean, variance, gauss_err
+        return None, None, None, None
 
         # # Distribute work amoung number of processes (self.size)
         # for _ in range(sample_num // self.size):
@@ -109,85 +137,29 @@ class MonteCarlo:
 
 # Check that the current script is being run directly as the amin program, or
 # if it's being imported as a module into another program.
-if __name__ == "__main__":
-    sim = MonteCarlo(seed=71)
+# if __name__ == "__main__":
+#     sim = MonteCarlo(seed=71)
 
-    # Generate the random numbers
-    random_num = sim.gen_ran_num()
-    # sys.stdout.flush()  # Manually buffer (otherwise it only prints rank 0)
-    # print(f"Process {sim.rank}: {random_num}")
+#     # Generate the random numbers
+#     random_num = sim.gen_ran_num()
+#     # sys.stdout.flush()  # Manually buffer (otherwise it only prints rank 0)
+#     # print(f"Process {sim.rank}: {random_num}")
 
-    # Loop to print the random numbers for each rank
-    for rank in range(sim.size):
-        if sim.rank == rank:
-            print(f"Process {sim.rank}: {random_num}")
-        # Ensure print in order
-        MPI.COMM_WORLD.Barrier()
+#     # Loop to print the random numbers for each rank
+#     for rank in range(sim.size):
+#         if sim.rank == rank:
+#             print(f"Process {sim.rank}: {random_num}")
+#         # Ensure print in order
+#         MPI.COMM_WORLD.Barrier()
 
-    if sim.rank == 0:
-        print("Simulation has started")
-        print("Start calculation for volume of hyperspaces:")
+#     if sim.rank == 0:
+#         print("Simulation has started")
+#         print("Start calculation for volume of hyperspaces:")
 
-    for dims in [2, 3, 4, 5]:
-        vol, vol_err = sim.mc_volume(dims)
-        if sim.rank == 0:
-            print(f"Estimated volume in {dims}D: {vol:.6f} error: {vol_err:.6f}")
-
-
+#     for dims in [2, 3, 4, 5]:
+#         vol, vol_err = sim.mc_volume(dims)
+#         if sim.rank == 0:
+#             print(f"Estimated volume in {dims}D: {vol:.6f} error: {vol_err:.6f}")
 
 
-
-
-
-
-
-# CODE FOR TASK 1
-# # Initializing communication in MPI
-# comm = MPI.COMM_WORLD
-# rank = comm.Get_rank()  # Get the process rank
-# size = comm.Get_size()  # Get the total number of processes
-
-# # Get the total number of ranks
-# nproc = comm.Get_size()
-
-# # The first processor is leader (rank 0), so one fewer available to be a worker
-# nworkers = nproc - 1
-
-# # Integral parameters
-# N = 100000  # Number of steps for integration
-# DEL = 1.0 / N  # Step size
-# INT = 0.0  # Integral
-
-
-# # Define function
-# def integrand(x_i):
-#     """Program to integrate the function to obtain the value of pi."""
-#     return 4.0 / (1.0 + x_i*x_i)
-
-# # NEW VERSION
-# # Divide work (N) amoung the ranks so that each calculates a partial sum of the integral
-# DIV_N = N // size
-# start = rank * DIV_N
-# end = start + DIV_N
-
-# time1 = time.time()
-# # Divides integral sum
-# DIV_INT = 0.0  # Initialize
-# for i in range(start, end):
-#     x = (i+0.5) *DEL
-#     DIV_INT += integrand(x) * DEL
-
-# # Combine all partial sums
-# TOTAL_INT = comm.reduce(DIV_INT, op=MPI.SUM, root=0)
-
-# # Print result from rank 0
-# if rank == 0:
-#     print(f"Integral = {TOTAL_INT:.15f}")
-
-# time2 = time.time()
-
-# time = time2 - time1
-
-# print("Total time (seconds) =", time)
-
-MPI.Finalize()
+# MPI.Finalize()
