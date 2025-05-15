Fixes and unit conversion
Implimented fixes for the random walk, fixed parenthasis for loc_walkers. Adding labels fro axis, adding missing parenthasis to show(),  fixed COMM typo, added unit conversion function.

Task_4/task4_code.py
@@ -56,9 +56,9 @@ class MonteCarlo:
         count_walkers = np.zeros((grid_size, grid_size))  # 2D array with zeros to record ammount of point visits
         x, y = start_xy
 
-        # Define edges of grid as boundary
-        boundary = [(0, i) for i in range(grid_size)] + [(grid_size - 1, i)for i in range(grid_size)] +\
-        [(i, 0) for i in range(grid_size)] + [(i, grid_size - 1)for i in range(grid_size)]
+        # # Define edges of grid as boundary
+        # boundary = [(0, i) for i in range(grid_size)] + [(grid_size - 1, i)for i in range(grid_size)] +\
+        # [(i, 0) for i in range(grid_size)] + [(i, grid_size - 1)for i in range(grid_size)]
         
         # Random walk until boundary, when boundary reached - end walk
         while (x, y) not in boundary:
@@ -70,7 +70,7 @@ class MonteCarlo:
                 x += 1
             elif step == "left" and y > 0:
                 y-= 1
-            elif step == "right" and y < 1:
+            elif step == "right" and y < grid_size - 1:
                 y += 1
             
         # Return count map of visited points
@@ -87,11 +87,15 @@ class MonteCarlo:
         """
         #Allocate walkers to processes
         loc_walkers = n_walkers // self.size
-        loc_count = np.zeros(grid_size, grid_size)
+        loc_count = np.zeros((grid_size, grid_size))
+        
+        # Define edges of grid as boundary
+        boundary = [(0, i) for i in range(grid_size)] + [(grid_size - 1, i)for i in range(grid_size)] +\
+        [(i, 0) for i in range(grid_size)] + [(i, grid_size - 1)for i in range(grid_size)]
         
         # 
         for _ in range(loc_walkers):
-            loc_count += self.rndm_walk(grid_size, start_xy)
+            loc_count += self.rndm_walk(grid_size, start_xy, boundary)
             
         # Combine in rank 0
         tot_count = self.comm.reduce(loc_count, op=MPI.SUM, root=0)
@@ -107,7 +111,6 @@ class MonteCarlo:
 def relaxation(grid_size, space, charge, boundary_cond, omega=1.8, iters=1000, tol=1e-5):
     """
     
-
     Parameters
     ----------
     grid_size : TYPE
@@ -148,6 +151,7 @@ def relaxation(grid_size, space, charge, boundary_cond, omega=1.8, iters=1000, t
                 
         # Check
         if np.max(np.abs(phi - old_phi)) < tol:
+        # if np.linalg.norm(phi - old_phi) < tol:
             break
         
     return phi

Task_4/task4_test.py
@@ -11,7 +11,7 @@ Licensed under the MIT License. See the LICENSE file in the repository for detai
 # Import
 from mpi4py import MPI
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 from task4_code import MonteCarlo
 
 # Plot of Green's functing estimated using Monte Carlo 
@@ -31,19 +31,25 @@ def plot_results(phi, green):
     None.
 
     """
-    fig, ax = plt.subplots(1, figsize=(12, 6))
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6))
     
-    pl1 = ax[0].imshow(green, cmap='viridis', origin='lower')
+    pl1 = ax1.imshow(green, cmap='viridis', origin='lower')
+    ax1.set_title("Green's Function")
+    ax1.set_xlabel("x")
+    ax1.set_ylabel("y")
+    
+    pl2 = ax2.imshow(phi, cmap='viridis', origin='lower')
     # ax[0].set_title
     # ax[0].set_xlabel
     # ax[0].set_ylabel
     
-    pl2 = ax[1].imshow(phi, cmap='viridis', origin='lower')
-    # ax[0].set_title
-    # ax[0].set_xlabel
-    # ax[0].set_ylabel
+    plt.show()
     
-    plt.show
+
+def units(x_cm, y_cm, grid_cm, N):
+    """Perform unit conversion from si to grid indices."""
+    leng = grid_cm / N  # Grid spacing
+    return int(x_cm / leng), int(y_cm / leng)  # Conversion
     
 def main():
     """
@@ -69,14 +75,14 @@ def main():
     }
     
     # Zero charge inside grid (f=0)
-    grid_charge = np.zeros((grid_size, grid_size))
+    charge = np.zeros((grid_size, grid_size))
     
     mc = MonteCarlo(seed=71)
     green_func = mc.green(grid_size, start_xy, n_walkers)
     
     # Ensure only rank 0 makes plot
-    if MPI.COM_WORLD.Get_rank() == 0:
-        phi = mc.relaxation(grid_size, space, grid_charge, boundary_cond)
+    if MPI.COMM_WORLD.Get_rank() == 0:
+        phi = mc.relaxation(grid_size, space, charge, boundary_cond)
         plot_results(green_func, phi)
         
 if __name__ == "__main__":